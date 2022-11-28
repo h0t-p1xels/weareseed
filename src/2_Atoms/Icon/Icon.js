e0e43f3a339688c74a819358b13355a2f572c9ad
Nikki Pantony <nikkipantony@gmail.com>
2022-11-28 22:21:53 +0000

    Add repo

---
+++ src/2_Atoms/Icon/Icon.js
@@ -0,0 +1,23 @@
+/*
+ * Icon Atom Component
+ *
+ */
+
+import React from "react"
+// import PropTypes from "prop-types"
+
+const Icon = (props) => {
+  return <>Icon</>
+}
+
+// Icon.defaultProps = {
+// text: "",
+// modifier: "",
+// }
+
+// Icon.propTypes = {
+// text: PropTypes.string.isRequired,
+// modifier: PropTypes.oneOf(["", "--something"]),
+// }
+
+export default Icon
---
+++ src/6_Pages/index.md
@@ -0,0 +1,11 @@
+---
+date: "2022-11-28"
+type: "page"
+path: "/"
+seoTitle: "SEED. Creating value through innovative digital experiences."
+link: "https://linkedIn.com/company/weareseed"
+---
+
+# SEED
+
+Creating value through innovative digital experiences.
