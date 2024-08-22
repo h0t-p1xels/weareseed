e094c4cc43a55d9e081d23857053c6429a50a440
h0t-p1xels <nikkipantony@gmail.com>
2024-08-22 03:35:22 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,11 +13,11 @@ export default defineConfig({
 		react(),
 		webmanifest({
 			// docs: https://github.com/alextim/astro-lib/tree/main/packages/astro-webmanifest#readme
-			name: "Nikki Pantony", // required
+			name: "SEED Studio", // required
 			icon: "./public/favicon.svg", // Favicon file name using automatic icon generation mode
-			short_name: "Nikki Pantony",
+			short_name: "SEED",
 			description:
-				"digital product designer & developer at weareseed.studio based in Whitley Bay, England, UK.",
+				"SEED Studio - Creating value through innovative digital experiences.",
 			start_url: "/", // index
 			theme_color: "#fefefe", // ally-light-50
 			background_color: "#0f172a", // ally-key-black-950
