d9ebc4368bd752f440c097ad7b4badf1b4681e3a
h0t-p1xels <nikkipantony@gmail.com>
2024-08-22 21:15:49 +0100

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -64,7 +64,7 @@ export const theme = {
 		// => @media (min-width: 1024px) { ... }
 		xl: "1280px",
 		// => @media (min-width: 1280px) { ... }
-		"2xl": "1536px"
+		xxl: "1536px"
 		// => @media (min-width: 1536px) { ... }
 	},
 	container: {
